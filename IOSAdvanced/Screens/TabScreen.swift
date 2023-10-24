4aafe3609eaf9d3dc2ef657b4c91c31c42f4baa3
BekDevIOS <umekhmonboev@gmail.com>
2023-10-24 17:34:32 +0900

    project is ready to submit!

---
+++ IOSAdvanced/Screens/TabScreen.swift
@@ -0,0 +1,18 @@
+//
+//  TabScreen.swift
+//  IOSAdvanced
+//
+//  Created by Utkirbek Mekhmonboev on 10/24/23.
+//
+
+import SwiftUI
+
+struct TabScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabScreen()
+}
